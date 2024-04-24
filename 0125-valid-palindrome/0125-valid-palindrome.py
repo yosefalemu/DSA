fbbe86f17fd2d0d23b188315437fb5341a76ea58
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2024-04-24 12:16:35 +0300

    Time: 77 ms (5.07%), Space: 17 MB (63.26%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -2,6 +2,9 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         tempStr = ""
         for eachItem in s:
-            if ord(eachItem) in range(65, 91) or ord(eachItem) in range(97, 123) or ord(eachItem) in range(48, 58):
+            if ord(eachItem) in range (65,91) or ord(eachItem) in range(97,123) or ord(eachItem) in range(48,58):
                 tempStr += eachItem.lower()
         return tempStr == tempStr[::-1]
+           
+                
+        
