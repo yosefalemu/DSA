7330d1f1ea86fa6a35e9d1b9d288512d9fec56ab
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2024-09-16 13:26:55 +0300

    Time: 29 ms (93.05%), Space: 16.6 MB (21.09%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,24 +1,14 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        added_number = [0]*len(digits)
-        added_number[len(digits) - 1] = 1
-        rightpt = len(digits) - 1
-        carry = 0
-        while rightpt >= 0:
-            current_sum = digits[rightpt] + added_number[rightpt] + carry
-            if current_sum == 10:
-                digits[rightpt] = 0
-                carry = 1
+        pointer = len(digits) - 1
+        while pointer >= 0:
+            if digits[pointer] == 9:
+                digits[pointer] = 0
             else:
-                digits[rightpt] = current_sum
-                carry = 0
-            rightpt -= 1
-        if carry:
-            ans = [1]
-            for item in digits:
-                ans.append(item)
-            return ans
-        return digits
+                digits[pointer] += 1
+                return digits
+            pointer -= 1
+        return [1] + digits
         
         
 
