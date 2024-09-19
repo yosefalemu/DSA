542f57fd54c243a4c71e9c35512ad64cfb9d71b5
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2024-09-19 21:50:49 +0300

    Time: 23 ms (99.80%), Space: 16.6 MB (8.69%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,14 +3,21 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        m = m - 1
+        n = n - 1
         for i in range(len(nums1) - 1, -1, -1):
-            # If nums2 is exhausted, just copy nums1
-            if n == 0:
-                break
-            # If nums1 is exhausted or nums2's element is greater, copy from nums2
-            if m == 0 or (n > 0 and nums2[n - 1] >= nums1[m - 1]):
-                nums1[i] = nums2[n - 1]
+            if m >= 0  and n >= 0:
+                if nums2[n] >= nums1[m]:
+                    nums1[i] = nums2[n]
+                    n -= 1
+                else:
+                    nums1[i] = nums1[m]
+                    m -= 1
+            elif m >= 0:
+                nums1[i] = nums1[m]
+                m -= 1
+            elif n >= 0:
+                nums1[i] = nums2[n]
                 n -= 1
             else:
-                nums1[i] = nums1[m - 1]
-                m -= 1
+                pass
