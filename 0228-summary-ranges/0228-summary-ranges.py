163bc1f0968926d4dbedc8f1b0c0b94b69a43644
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2024-09-22 20:48:47 +0300

    Time: 37 ms (39.30%), Space: 16.5 MB (69.42%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -2,21 +2,19 @@ class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
         if not nums:
             return []
-        rangeArr = [0]
+        start = 0
         ans = []
         for i in range(1,len(nums)):
-            if nums[i] == nums[i - 1] + 1:
-                rangeArr.append(i)
-            else:
-                if len(rangeArr) == 1:
-                    ans.append(str(nums[rangeArr[0]]))
+            if nums[i] != nums[i - 1] + 1:
+                if start == i - 1:
+                    ans.append(str(nums[start]))
                 else:
-                    ans.append(f"{nums[rangeArr[0]]}->{nums[rangeArr[-1]]}")
-                rangeArr = [i]
-        if len(rangeArr) == 1:
-            ans.append(str(nums[rangeArr[0]]))
+                    ans.append(f"{nums[start]}->{nums[i - 1]}")
+                start = i
+        if start == len(nums) - 1:
+            ans.append(str(nums[start]))
         else:
-            ans.append(f"{nums[rangeArr[0]]}->{nums[rangeArr[-1]]}")
+            ans.append(f"{nums[start]}->{nums[-1]}")   
         return ans
         
                     
