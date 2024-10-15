532fdf0cefb7993e64c3a2fec265d34df4b0580a
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2024-10-15 19:49:11 +0300

    Time: 240 ms (87.45%), Space: 17.3 MB (88.97%) - LeetHub

---
+++ 0731-my-calendar-ii/0731-my-calendar-ii.py
@@ -0,0 +1,22 @@
+class MyCalendarTwo:
+
+    def __init__(self):
+        self.over_lapping = []
+        self.non_over_lapping = []
+        
+
+    def book(self, start: int, end: int) -> bool:
+        for s, e in self.over_lapping:
+            if not (end <= s or e <= start):
+                return False
+        for s,e in self.non_over_lapping:
+            if not (end <= s or e<= start):
+                self.over_lapping.append((max(s,start),min(e,end)))
+        self.non_over_lapping.append((start,end))
+        return True
+        
+
+
+# Your MyCalendarTwo object will be instantiated and called as such:
+# obj = MyCalendarTwo()
+# param_1 = obj.book(start,end)
