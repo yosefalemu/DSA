cbcba2f669bc8d3743b2eef0a65e6d3597e3d16e
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2024-10-18 23:45:38 +0300

    Time: 2 ms (100%), Space: 16.5 MB (42.33%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,12 +1,18 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        visit_set = set()
-        while n not in visit_set:
-            visit_set.add(n)
-            n = self.findSquareOfEach(n)
-            if n == 1:
-                return True
-        return False
+        # visit_set = set()
+        # while n not in visit_set:
+        #     visit_set.add(n)
+        #     n = self.findSquareOfEach(n)
+        #     if n == 1:
+        #         return True
+        # return False
+        slow = n
+        fast = self.findSquareOfEach(n)
+        while fast != 1 and slow != fast:
+            slow = self.findSquareOfEach(slow)
+            fast = self.findSquareOfEach(self.findSquareOfEach(fast))
+        return fast == 1
     def findSquareOfEach(self,n:int)->int:
         current_sum = 0
         while n:
