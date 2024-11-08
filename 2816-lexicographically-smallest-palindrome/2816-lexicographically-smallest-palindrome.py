70f532c80cb8cb9d193df9719d483e5295877881
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2024-11-08 23:16:26 +0300

    Time: 76 ms (29.39%), Space: 16.8 MB (39.25%) - LeetHub

--- 2816-lexicographically-smallest-palindrome/2816-lexicographically-smallest-palindrome.py
+++ 2816-lexicographically-smallest-palindrome/2816-lexicographically-smallest-palindrome.py
@@ -5,7 +5,8 @@ class Solution:
         listOfS = list(s)
         while leftPt < rightPt:
             if listOfS[leftPt] != listOfS[rightPt]:
-                listOfS[leftPt] ,listOfS[rightPt] = min(s[leftPt],s[rightPt]), min(s[leftPt],s[rightPt])
+                min_char = min(s[leftPt],s[rightPt])
+                listOfS[leftPt] ,listOfS[rightPt] = min_char, min_char 
             leftPt += 1
             rightPt -= 1
         return "".join(listOfS)
