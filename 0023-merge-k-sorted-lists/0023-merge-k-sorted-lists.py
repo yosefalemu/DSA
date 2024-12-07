f8e50e920bb3a9e00231f6c30d5fb7367c2cd2e7
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2024-12-07 23:38:04 +0300

    Time: 7 ms (91.34%), Space: 19.7 MB (26.65%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -1,23 +1,36 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        # Собираем все узлы в один список
-        all_nodes = []
-        for lst in lists:
-            while lst:
-                all_nodes.append(lst)
-                lst = lst.next
-        
-        # Сортируем список узлов по значению val
-        all_nodes.sort(key=lambda x: x.val)
-        
-        # Создаем новую голову
+        if not lists and len(lists) == 0:
+            return None
+        while len(lists) > 1:
+            temp = []
+            for i in range(0,len(lists),2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if i < len(lists) - 1 else None
+                temp.append(self.mergeTwoLinkedList(l1,l2))
+            lists = temp
+        return lists[0]
+
+    def mergeTwoLinkedList(self,list1,list2)->Optional[ListNode]:
         dummy = ListNode()
-        current = dummy
-        
-        # Связываем узлы в порядке возрастания значения val
-        for node in all_nodes:
-            current.next = node
-            current = current.next
-        
+        curr = dummy
+        while list1 and list2:
+            if list1.val <= list2.val:
+                curr.next = list1
+                list1 = list1.next
+            else:
+                curr.next = list2
+                list2 = list2.next
+            curr = curr.next
+        if list1:
+            curr.next = list1
+        if list2:
+            curr.next = list2
         return dummy.next
+            
+        
