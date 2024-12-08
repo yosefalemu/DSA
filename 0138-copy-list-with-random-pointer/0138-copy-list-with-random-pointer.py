f52ba93dd7f2cb7f412650f9b06d752239a30a1b
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2024-12-08 19:09:02 +0300

    Time: 26 ms (98.8%), Space: 18.2 MB (5.88%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -8,22 +8,16 @@ class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if not head:
             return None
-        
-        # Step 1: Create a mapping from original nodes to copied nodes
-        old_to_new = {}
+        deepCopy = {}
         curr = head
         while curr:
-            old_to_new[curr] = Node(curr.val)
+            deepCopy[curr] = Node(curr.val)
             curr = curr.next
-        
-        # Step 2: Assign the next and random pointers for the copied nodes
         curr = head
         while curr:
             if curr.next:
-                old_to_new[curr].next = old_to_new[curr.next]
+                deepCopy[curr].next = deepCopy[curr.next]
             if curr.random:
-                old_to_new[curr].random = old_to_new[curr.random]
+                deepCopy[curr].random = deepCopy[curr.random]
             curr = curr.next
-        
-        # Return the deep-copied head
-        return old_to_new[head]
+        return deepCopy[head]
