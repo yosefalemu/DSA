f026ef9c57d9a2c1e571500faebc6e524062e69e
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2024-12-08 18:26:43 +0300

    Time: 82 ms (10.56%), Space: 18.2 MB (14.95%) - LeetHub

--- 0838-design-linked-list/0838-design-linked-list.py
+++ 0838-design-linked-list/0838-design-linked-list.py
@@ -1,12 +1,15 @@
 class ListNode:
-    def __init__(self,val):
+    def __init__(self, val):
         self.val = val
         self.next = None
+
+
 class MyLinkedList:
 
     def __init__(self):
         self.head = None
         self.size = 0
+
     def get(self, index: int) -> int:
         if index > self.size - 1 or index < 0:
             return -1
@@ -17,27 +20,27 @@ class MyLinkedList:
             curr = curr.next
 
     def addAtHead(self, val: int) -> None:
-        self.addAtIndex(0,val)
-        
+        self.addAtIndex(0, val)
 
     def addAtTail(self, val: int) -> None:
-        self.addAtIndex(self.size,val)
-        
+        self.addAtIndex(self.size, val)
 
     def addAtIndex(self, index: int, val: int) -> None:
         if index > self.size:
             return
         newNode = ListNode(val)
-        curr = self.head
-        if index <= 0:
-            newNode.next = curr
+        if index == 0:
+            newNode.next = self.head
             self.head = newNode
-        else:
-            for _ in range(index - 1):
-                curr = curr.next
-            newNode.next = curr.next
-            curr.next = newNode
+            self.size += 1
+            return
+        curr = self.head
+        for _ in range(index - 1):
+            curr = curr.next
+        newNode.next = curr.next
+        curr.next = newNode
         self.size += 1
+
     def deleteAtIndex(self, index: int) -> None:
         if index < 0 or index > self.size - 1:
             return
@@ -50,6 +53,7 @@ class MyLinkedList:
             self.head = self.head.next
         self.head = dummy.next
         self.size -= 1
+
 
 # Your MyLinkedList object will be instantiated and called as such:
 # obj = MyLinkedList()
