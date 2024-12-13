7b5ada592f6630969f7e41fa253fcb25c74d7ff9
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2024-12-13 16:20:01 +0300

    Time: 3 ms (10.48%), Space: 17.4 MB (6.89%) - LeetHub

--- 0904-leaf-similar-trees/0904-leaf-similar-trees.py
+++ 0904-leaf-similar-trees/0904-leaf-similar-trees.py
@@ -15,23 +15,8 @@ class Solution:
                 leafNodes.append(node.val)
             traverseTree(node.left,leafNodes)
             traverseTree(node.right,leafNodes)
-        # def traverseTree2(node):
-        #     if not node:
-        #         return
-        #     if not node.left and not node.right:
-        #         leafNodes2.append(node.val)
-        #     traverseTree2(node.left)
-        #     traverseTree2(node.right)
         traverseTree(root1,leafNodes1)
         traverseTree(root2,leafNodes2)
         return leafNodes1 == leafNodes2
-        # length1 = len(leafNodes1)
-        # length2 = len(leafNodes2)
-        # if length1 != length2:
-        #     return False
-        # for i in range(length1):
-        #     if leafNodes1[i] != leafNodes2[i]:
-        #         return False
-        # return True
 
         
