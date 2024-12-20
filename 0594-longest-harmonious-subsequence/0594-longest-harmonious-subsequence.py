5f8f64fa8e35a8d731e083fc2ecdac3a1d7d856b
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2024-12-20 16:31:08 +0300

    Time: 18 ms (91.44%), Space: 19.1 MB (5.19%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
@@ -1,13 +1,13 @@
 class Solution:
     def findLHS(self, nums: List[int]) -> int:
         freq_map = {}
-        for num in nums:
-            if num in freq_map:
-                freq_map[num] += 1
+        for c in nums:
+            if c in freq_map:
+                freq_map[c] += 1
             else:
-                freq_map[num] = 1
+                freq_map[c] = 1
         ans = 0
-        for key,val in freq_map.items():
+        for key, val in freq_map.items():
             if key + 1 in freq_map:
                 ans = max(ans,val + freq_map[key + 1])
         return ans
