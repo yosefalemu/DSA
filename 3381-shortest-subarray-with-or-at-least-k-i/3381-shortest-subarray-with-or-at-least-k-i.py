381530e5bf47026d76f73813bd225e29849c07f5
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2024-12-26 16:34:13 +0300

    Time: 6 ms (69.02%), Space: 17.8 MB (6.06%) - LeetHub

--- 3381-shortest-subarray-with-or-at-least-k-i/3381-shortest-subarray-with-or-at-least-k-i.py
+++ 3381-shortest-subarray-with-or-at-least-k-i/3381-shortest-subarray-with-or-at-least-k-i.py
@@ -1,14 +1,13 @@
 class Solution:
     def minimumSubarrayLength(self, nums: List[int], k: int) -> int:
         ans = inf
-        intial = 0
         for i in range(len(nums)):
+            intial = 0
             for j in range(i,len(nums)):
-                intial = intial | nums[j]
+                intial |= nums[j] 
                 if intial >= k:
                     ans = min(ans,j - i + 1)
                     break
-            intial = 0
         return ans if ans != inf else -1
                 
                  
