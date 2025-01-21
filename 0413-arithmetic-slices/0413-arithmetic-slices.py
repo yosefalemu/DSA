e3505dfad44fd16b8e1a697c1f8dce2b2ca52923
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2025-01-21 15:37:03 +0300

    Time: 1 ms (24.82%), Space: 17.9 MB (43.27%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.py
+++ 0413-arithmetic-slices/0413-arithmetic-slices.py
@@ -1,12 +1,16 @@
 class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
         n = len(nums)
-        dp = [0]*n
-        ans = 0
+        if n < 3:
+            return 0
+        current = 0
+        total = 0
         for i in range(2,n):
             if nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]:
-                dp[i] = dp[i - 1] + 1
-                ans += dp[i]
-        return ans
+                current += 1
+                total += current
+            else:
+                current = 0
+        return total
 
         
