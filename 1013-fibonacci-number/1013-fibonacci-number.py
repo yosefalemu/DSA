acafc3269a3db2ce967abe469143616fcebec467
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2025-01-29 23:27:19 +0300

    Time: 33 ms (84.95%), Space: 17.7 MB (47.41%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,18 +1,12 @@
 class Solution:
     def fib(self, n: int) -> int:
-        memo = {}
-        return self.fibImp(n,memo)
-    def fibImp(self,n,memo) -> int:
-        if n in memo:
-            return memo[n]
-        if n == 0:
-            return 0
-        if n == 1:
-            return 1
-        left = self.fibImp(n - 1,memo)
-        right = self.fibImp(n - 2,memo)
-        memo[n] = left + right
-        return memo[n]
+        dp = [0]*(n+1)
+        for i in range(n + 1):
+            if i == 0 or i == 1:
+                dp[i] = i
+            else:
+                dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[-1]
 
     
 
