1b0585bbb33e94b3044add2a1feb5eed49397f34
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2025-02-08 20:57:42 +0300

    Time: 0 ms (100%), Space: 17.8 MB (26.38%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,15 +1,17 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n == 1:
-            return 1
-        if n == 2:
-            return 2
-        left = 1
-        right = 2
-        for stairCase in range(3,n + 1):
-            ans = left + right
-            left, right = right, left + right
-        return ans
+        memo = {}
+        def helper(number) -> int:
+            if number in memo:
+                return memo[number]
+            if number == n:
+                return 1
+            if number > n:
+                return 0
+            memo[number] = helper(number + 1) + helper(number + 2)
+            return memo[number]
+        return helper(0)
+
 
 
         
