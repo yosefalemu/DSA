b879627a77d3024416900716817db718ceaf7f2c
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2025-02-14 09:28:58 +0300

    Time: 40 ms (21.67%), Space: 18.7 MB (7.5%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,16 +1,9 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        if n == 0:
-            return [0]
-        if n == 1:
-            return [0,1]
         ans = [0]*(n+1)
-        ans[1] = ans[2] = 1
-        currPowerTwo = 2
-        for i in range(2,len(ans)):
-            if math.log2(i).is_integer():
-                currPowerTwo = i
-                ans[i] = 1
-            else:
-                ans[i] = ans[currPowerTwo] + ans[i - currPowerTwo]
+        currPowerTwo = 1
+        for i in range(1,n+1):
+            if i == currPowerTwo*2:
+                currPowerTwo = currPowerTwo*2
+            ans[i] = 1 + ans[i - currPowerTwo]
         return ans
