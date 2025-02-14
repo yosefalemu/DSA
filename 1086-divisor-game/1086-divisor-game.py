f8ca2811408f5d3c7a3c6eb18fe237eb02248b9b
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2025-02-14 10:17:20 +0300

    Time: 0 ms (100%), Space: 17.8 MB (38.99%) - LeetHub

--- 1086-divisor-game/1086-divisor-game.py
+++ 1086-divisor-game/1086-divisor-game.py
@@ -1,9 +1,5 @@
 class Solution:
     def divisorGame(self, n: int) -> bool:
-        turn = 0
-        while n > 1:
-            n -= 1
-            turn += 1
-        return not turn % 2 == 0
+        return n % 2 == 0
         
         
