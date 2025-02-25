3f4173d009494e5b301956fddabca1026ea11828
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2025-02-25 19:18:57 +0300

    Time: 35 ms (87.72%), Space: 32.6 MB (45.58%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -3,16 +3,10 @@ class Solution:
         ans = [0]*len(nums)
         ans[0] = nums[0]
         for i in range(1,len(nums)):
-            if nums[i] >= 0:
-                if ans[i - 1] >= 0:
-                    ans[i] = ans[i - 1] + nums[i]
-                else:
-                    ans[i] = nums[i]
+            if ans[i - 1] >= 0:
+                ans[i] = ans[i - 1] + nums[i]
             else:
-                if ans[i - 1] >= 0:
-                    ans[i] = ans[i - 1] + nums[i]
-                else:
-                    ans[i] = nums[i]
+                ans[i] = nums[i]
         return max(ans)
 
         
