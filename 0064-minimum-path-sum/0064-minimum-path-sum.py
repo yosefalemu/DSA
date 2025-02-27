78764224f70c1947fb2d202e5ae51e5b12cf6f3f
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2025-02-27 19:11:38 +0300

    Time: 15 ms (59.7%), Space: 20 MB (73.74%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,10 +1,13 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
+        # Set the first column
         for i in range(1,rows):
             grid[i][0] += grid[i - 1][0]
+        # Set the first row
         for j in range(1,cols):
             grid[0][j] += grid[0][j - 1]
+        # Fill other slots accordingly
         for r in range(1, rows):
             for c in range(1, cols):
                 grid[r][c] += min(grid[r][c - 1],grid[r - 1][c])
