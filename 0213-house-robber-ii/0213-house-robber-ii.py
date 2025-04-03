84dbeefe8ed715e1c4610cb33220493e879b97e9
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2025-04-03 19:15:37 +0300

    Time: 0 ms (100%), Space: 17.9 MB (38.29%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -5,20 +5,17 @@ class Solution:
             return nums[0]
         if n == 2:
             return max(nums)
-        index = 0
         dp1 = [0]*(n - 1)
-        if index == 0:
-            dp1[0] = nums[0]
-            dp1[1] = max(nums[0],nums[1])
-            for i in range(2, n - 1):
-                dp1[i] = max(dp1[i - 2] + nums[i], dp1[i - 1])
+        dp1[0] = nums[0]
+        dp1[1] = max(nums[0],nums[1])
+        for i in range(2, n - 1):
+            dp1[i] = max(dp1[i - 2] + nums[i], dp1[i - 1])
         dp2 = [0]*(n - 1)
         nums.reverse()
-        if index == 0:
-            dp2[0] = nums[0]
-            dp2[1] = max(nums[0],nums[1])
-            for i in range(2, n - 1):
-                dp2[i] = max(dp2[i - 2] + nums[i], dp2[i - 1])
+        dp2[0] = nums[0]
+        dp2[1] = max(nums[0],nums[1])
+        for i in range(2, n - 1):
+            dp2[i] = max(dp2[i - 2] + nums[i], dp2[i - 1])
         return max(dp1[-1], dp2[-1])
         
 
