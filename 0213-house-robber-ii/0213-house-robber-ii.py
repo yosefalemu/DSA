9d03c7ec0817266829521ee51d932cbce794fb9b
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2025-04-03 19:23:58 +0300

    Time: 1 ms (16.46%), Space: 17.8 MB (38.29%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -5,18 +5,11 @@ class Solution:
             return nums[0]
         if n == 2:
             return max(nums)
-        dp1 = [0]*(n - 1)
-        dp1[0] = nums[0]
-        dp1[1] = max(nums[0],nums[1])
-        for i in range(2, n - 1):
-            dp1[i] = max(dp1[i - 2] + nums[i], dp1[i - 1])
-        dp2 = [0]*(n - 1)
-        nums.reverse()
-        dp2[0] = nums[0]
-        dp2[1] = max(nums[0],nums[1])
-        for i in range(2, n - 1):
-            dp2[i] = max(dp2[i - 2] + nums[i], dp2[i - 1])
-        return max(dp1[-1], dp2[-1])
-        
-
-        
+        def findMaxRob(arrNum):
+            dp = [0]*(n - 1)
+            dp[0] = arrNum[0]
+            dp[1] = max(arrNum[0],arrNum[1])
+            for i in range(2, n - 1):
+                dp[i] = max(dp[i - 2] + arrNum[i], dp[i - 1])
+            return dp[-1]
+        return max(findMaxRob(nums),findMaxRob(nums[::-1]))
