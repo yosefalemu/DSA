91d527e449703e9ec22078c1a185bbef54c9fa92
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2025-04-10 17:48:21 +0300

    Time: 22 ms (14.4%), Space: 18.1 MB (20.26%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        def helper(start, tempL):
+            if sum(tempL) == target:
+                ans.append(tempL[:])
+                return
+            if sum(tempL) > target:
+                return
+            for i in range(start,len(candidates)):
+                tempL.append(candidates[i])
+                helper(i,tempL)
+                tempL.pop()    
+        ans = []
+        helper(0,[])
+        return ans
