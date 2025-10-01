0a782210750205eb6884c9a2830a51904fa50cde
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2025-10-01 23:51:27 +0300

    Update 0001-two-sum.py

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,5 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #simple
         seen = {}
         for i in range(len(nums)):
             diff = target - nums[i]
