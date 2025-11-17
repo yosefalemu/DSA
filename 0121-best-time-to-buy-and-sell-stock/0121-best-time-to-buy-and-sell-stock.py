24e1b819bf3d23b942e9b7eaef219b3c09ac8a3a
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2025-11-17 10:39:37 +0300

    0121-BEST-TIME-TO-BUY-AND-SELL-STOCK Stats: Time: 115 ms (38.35%), Space: 27 MB (32.93%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,12 +1,17 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        buyPrice = prices[0]
-        profit = 0
-        for i in range(1,len(prices)):
-            if prices[i] < buyPrice:
-                buyPrice = prices[i]
-            elif profit < prices[i] - buyPrice:
-                profit = prices[i] - buyPrice
-        return profit
-
-            
+        # Take the first price as min_price
+        min_price = prices[0]
+        # Take the max profit as 0
+        max_profit = 0
+        # Iterate over the prices array
+        for price in prices:
+            # Determine what will be my profit if i sell the item today
+            profit = price - min_price
+            # Maximize the profit based on my current profit
+            max_profit = max(max_profit, profit)
+            # Take the currenet price as min if it is less than the min_price
+            min_price = min(min_price, price)
+        # Return the max_profit
+        return max_profit
+        
