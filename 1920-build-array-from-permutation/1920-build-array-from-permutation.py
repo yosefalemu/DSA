bae932512995fb09551124911f9b20241c8369d4
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2025-11-25 09:22:57 +0300

    Time: 3 ms (46.32%), Space: 18 MB (56.66%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.py
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.py
@@ -1,10 +1,14 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
         n = len(nums)
+        #Decode
         for i in range(n):
             nums[i] += n*(nums[nums[i]] % n)
+        #Encode
         for i in range(n):
             nums[i] //= n
         return nums
+
+
 
         
