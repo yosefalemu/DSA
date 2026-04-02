1873fdec4c29806124a49d67e439678b00a3010c
Yosef A. <125744573+yosefalemu@users.noreply.github.com>
2026-04-02 08:39:58 +0300

    Time: 25 ms (11.53%), Space: 20.1 MB (94.7%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -1,21 +1,17 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
-        vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
-        s = list(s)
-        leftPt = 0
-        rightPt = len(s) - 1
-        while leftPt < rightPt:
-            if s[leftPt] in vowels and s[rightPt] in vowels:
-                s[leftPt],s[rightPt] = s[rightPt],s[leftPt]
-                leftPt += 1
-                rightPt -= 1
-            elif s[leftPt] in vowels and s[rightPt] not in vowels:
-                rightPt -= 1
-            elif s[leftPt] not in vowels and s[rightPt] in vowels:
-                leftPt += 1
+        vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+        temp = []
+        for char in s:
+            if char in vowels:
+                temp.append(char)
+        temp_index = len(temp) - 1
+        ans = ""
+        for i in range(len(s)):
+            if s[i] in vowels:
+                ans += temp[temp_index]
+                temp_index -= 1
             else:
-                leftPt += 1
-                rightPt -= 1
-        return "".join(s)
-        
+                ans += s[i]
+        return ans
         
