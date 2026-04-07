a13148887e85bfc488ea46ab6204718ad38e557a
Yosef A. <125744573+yosefalemu@users.noreply.github.com>
2026-04-07 09:09:31 +0300

    Time: 55 ms (59.42%), Space: 19.7 MB (95.01%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -7,9 +7,7 @@ class Solution:
                 curr_count += 1
         max_count = curr_count
         for i in range(k, len(s)):
-            vowel_inc = s[i] in vowels
-            vowel_dec = s[i - k] in vowels
-            curr_count += (1 if vowel_inc else 0) + (-1 if vowel_dec else 0)
+            curr_count += (1 if s[i] in vowels else 0) + (-1 if s[i - k] in vowels else 0)
             max_count = max(max_count, curr_count)
         return max_count
 
