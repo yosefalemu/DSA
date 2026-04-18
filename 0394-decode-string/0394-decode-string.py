2fca3f250decafd34dee8b4da70a18720c184c5e
Yosef A. <125744573+yosefalemu@users.noreply.github.com>
2026-04-18 08:43:04 +0300

    Time: 0 ms (100%), Space: 19.4 MB (48.17%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,19 +1,22 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        stack = []
+        result = []
         for ch in s:
             if ch != "]":
-                stack.append(ch)
+                result.append(ch)
             else:
-                current_substring = ""
-                while stack and stack[-1] != "[":
-                    current_substring = stack.pop() + current_substring
-                stack.pop()
-                current_multiplier = ""
-                while stack and stack[-1].isdigit():
-                    current_multiplier = stack.pop() + current_multiplier
-                stack.append(current_substring * int(current_multiplier))
-        return "".join(stack)
+                curr_sub_string = ""
+                while result and result[-1] !="[":
+                    curr_sub_string = result.pop() + curr_sub_string
+                # Remove the [ from the stack
+                result.pop()
+                curr_multiplier = ""
+                while result and result[-1].isdigit():
+                    curr_multiplier = result.pop() + curr_multiplier
+                result.append(curr_sub_string*int(curr_multiplier))
+        return "".join(result)
+
+
 
 
 
