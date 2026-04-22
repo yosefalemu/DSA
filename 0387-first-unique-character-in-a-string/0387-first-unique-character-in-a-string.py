933cd94e2602c73e67fe21a0a5d02ab03a4eaa9b
Yosef A. <125744573+yosefalemu@users.noreply.github.com>
2026-04-22 08:50:02 +0300

    Time: 21 ms (99.36%), Space: 19.8 MB (11%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -1,7 +1,7 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
         counts = Counter(s)
-
+        # We only iterate a maximum of 26 characters
         for char, count in counts.items():
             if count == 1:
                 return s.find(char)
