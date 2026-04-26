187febd26093964afcec02b37fd0576c4136a9bf
Yosef A. <125744573+yosefalemu@users.noreply.github.com>
2026-04-27 00:23:23 +0300

    Time: 6 ms (2.85%), Space: 19.3 MB (73.85%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -3,15 +3,15 @@ class Solution:
         ans = strs[0]
         for i in range(1, len(strs)):
             leftpt, rightpt = 0, 0
-            common = ""
+            common = []
             while leftpt < len(ans) and rightpt < len(strs[i]):
                 if ans[leftpt] == strs[i][rightpt]:
-                    common += ans[leftpt]
+                    common.append(ans[leftpt])
                 else:
                     break
                 leftpt += 1
                 rightpt += 1
-            ans = common
+            ans = "".join(common)
             if ans == "":
                 return ""
         return ans
