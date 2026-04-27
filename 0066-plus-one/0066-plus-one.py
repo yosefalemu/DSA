be37c85d96268e5148f4bcd98996ca8e0b2dd1c9
Yosef A. <125744573+yosefalemu@users.noreply.github.com>
2026-04-27 07:58:24 +0300

    Time: 0 ms (100%), Space: 19.4 MB (20.98%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,11 +1,11 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        temp = 0
-        multiplier = 1
         for i in range(len(digits) - 1, -1, -1):
-            temp += multiplier*digits[i]
-            multiplier *= 10
-        return [int(num) for num in list(str(temp + 1))]
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        return [1] + digits
 
 
 
