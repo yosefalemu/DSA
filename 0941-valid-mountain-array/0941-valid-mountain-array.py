241cdc0f9c19128051d12e373a73ae12caeede2e
Yosef A. <125744573+yosefalemu@users.noreply.github.com>
2026-04-28 15:35:04 +0300

    Time: 148 ms (97.38%), Space: 20.7 MB (18.59%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.py
+++ 0941-valid-mountain-array/0941-valid-mountain-array.py
@@ -5,15 +5,14 @@ class Solution:
         max_index = arr.index(max(arr))
         if max_index == 0 or max_index == len(arr) - 1:
             return False
-
         for i in range(1, max_index + 1):
             if arr[i] <= arr[i - 1]:
                 return False
-        
         for i in range(max_index + 1, len(arr)):
             if arr[i] >= arr[i - 1]:
                 return False
         return True
+
 
 
 
